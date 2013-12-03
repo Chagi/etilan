d0feda66922316e31a439e5b7a7fd28575dadceb
Elias Forsberg <forsberg.elias@gmail.com>
2013-12-03 15:51:31 +0100

    'Merge' PointVector from 'https://www.github.com/hexointed/thuquhux

---
+++ Geometry/PointVector.h
@@ -0,0 +1,182 @@
+/* 
+ * File:   PointVector.h
+ * Author: Elias Forsberg
+ *
+ * Created on December 26, 2012, 4:19 PM
+ * 
+ * This class is hoped to be useful for representing N-dimensional points and 
+ * vectors. It includes several arithmetic operations that are common for vectors.
+ */
+
+#ifndef POINTVECTOR_H
+#define	POINTVECTOR_H
+
+#include <array>
+
+/*
+ * Dim is the dimension of the PointVector, and Numberic is the type used to represent
+ * the composants. Numeric should overload all arithmetic and comparison operators, 
+ * as well as = . It should also have a default constructor.
+ */
+
+template<int Dim = 3, typename Numeric = double>
+class PointVector{
+    
+public:
+	PointVector();
+	PointVector(Numeric composants[Dim]);
+	
+	template<int D2>
+	PointVector(PointVector<D2> orig);
+	
+	template<typename... Tail>
+	PointVector(Tail... t);
+
+private:
+	/*
+	 *  Constructor helper functions (for the variadic template constructor)
+	 */
+	template<typename First, typename... Tail>
+	inline void pconstruct(int i, First f, Tail... t);
+	inline void pconstruct(int i);
+    
+private:
+	Numeric comp[Dim];
+	class Cross_product;
+    
+public:
+	/*
+	 * Returns composants of the PointVector<Dim>. 
+	 * x,y and z represent comp[0], comp[1] and comp[2], respectively. 
+	 */
+	Numeric get(int i) const;
+	Numeric getdx() const;
+	Numeric getdy() const;
+	Numeric getdz() const;
+	
+	Numeric getMagnitude() const;
+	Numeric sum_comp() const;
+	
+	void set(int i, Numeric d);
+	void setdx(Numeric d);
+	void setdy(Numeric d);
+	void setdz(Numeric d);
+	
+	/*
+	 * Some common arithmetic operations. There exist several versions of many
+	 * of these operations; as operators, member methods, and static methods.
+	 */
+	
+	inline PointVector operator +(const PointVector r) const;
+	inline PointVector operator -(const PointVector r) const;
+	inline PointVector operator -() const;
+	
+	inline PointVector& operator+=(const PointVector r);
+	inline PointVector& operator-=(const PointVector r);
+	inline PointVector& operator*=(const Numeric r);
+	inline PointVector& operator/=(const Numeric r);
+	
+	template<int D, typename Num>
+	friend PointVector<D, Num> operator *(const PointVector<D, Num> l, const Num r);
+	template<int D, typename Num>
+	friend PointVector<D, Num> operator *(const Num l, const PointVector<D, Num> r);
+	template<int D, typename Num>
+	friend PointVector<D, Num> operator /(const PointVector<D, Num> l, const Num r);
+	template<int D, typename Num>
+	friend PointVector<D, Num> operator /(const Num l, const PointVector<D, Num> r);
+	
+	PointVector& add(PointVector p);
+	PointVector& sub(PointVector p);
+	static PointVector add(PointVector p, PointVector q);
+	static PointVector sub(PointVector p, PointVector q);
+	
+	PointVector& mul_comp(PointVector p);
+	PointVector& mul_cross(PointVector p);
+	PointVector& mul(Numeric d);
+	static Numeric mul_dot(PointVector p, PointVector q);
+	static PointVector mul_comp(PointVector p, PointVector q);
+	static PointVector mul_cross(PointVector p, PointVector q);
+	static PointVector mul(Numeric d, PointVector p);
+	
+	PointVector& div_comp(PointVector p);
+	PointVector& div(Numeric d);
+	static PointVector div_comp(PointVector p, PointVector q);
+	static PointVector div(Numeric d, PointVector p);
+	
+	PointVector& pow(Numeric d);
+	PointVector& pow_comp(PointVector p);
+	static PointVector pow(Numeric d, PointVector p);
+	static PointVector pow_comp(PointVector p, PointVector q);
+	
+	/*
+	 * a is considered max_comp(b) if all components of a are equal or bigger
+	 * than their respective component in b
+	 */
+	
+	PointVector& set_min_comp(PointVector p);
+	PointVector& set_max_comp(PointVector p);
+	static PointVector min_comp(PointVector p, PointVector q);
+	static PointVector max_comp(PointVector p, PointVector q);
+	
+	bool is_min_comp(PointVector p) const;
+	bool is_max_comp(PointVector p) const;
+	bool is_eq_comp (PointVector p) const;
+	
+	/*
+	 * A unit vector is a vector with the magnitude 1. make_unit() and 
+	 * make_unit(p) scale down their vectors so that their magnitude is one,
+	 * but the relative sizes of the composants remain.
+	 */
+	PointVector& make_unit();
+	static PointVector make_unit(PointVector p);
+	
+	/*
+	 * reflect creates a PointVector that is a reflection on the surface of which
+	 * normal is the normal vector. Projecting a PointVector p onto q creates a 
+	 * PointVector that is the component of p in the direction of q.
+	 */
+	PointVector& reflect(PointVector normal);
+	PointVector& project(PointVector p);
+	static PointVector reflect(PointVector p, PointVector normal);
+	static PointVector project(PointVector p, PointVector q);
+	static Numeric taxicab_distance(PointVector p, PointVector q);
+	
+	std::array<bool, Dim> operator==	(PointVector p) const;
+	std::array<bool, Dim> operator!=	(PointVector p) const;
+	std::array<bool, Dim> operator> 	(PointVector p) const;
+	std::array<bool, Dim> operator< 	(PointVector p) const;
+	std::array<bool, Dim> operator>=	(PointVector p) const;
+	std::array<bool, Dim> operator<=	(PointVector p) const;
+	
+	/*
+	 * Cross_product is a class that handles computation of cross products by using 
+	 * automatic type casting to the expected return type. This method is used because
+	 * of the lack of partial specialization of template class methods.
+	 */
+	
+private:
+	class Cross_product{
+	public:
+		Cross_product(PointVector t, PointVector u);
+		
+		operator PointVector<3,Numeric> () const;
+		operator PointVector<7,Numeric> () const;
+	
+	private:
+		PointVector q, p;
+	};
+	
+};
+
+namespace Boolarr{
+	template<int Dim>
+	bool all(std::array<bool, Dim> a);
+	template<int Dim>
+	bool any(std::array<bool, Dim> a);
+	template<int Dim>
+	bool most(std::array<bool, Dim> a);
+}
+
+#endif	/* POINTVECTOR_H */
+
+#include "PointVector.hpp"
---
+++ Geometry/PointVector.hpp
@@ -0,0 +1,516 @@
+/* 
+ * File:   PointVector.cpp
+ * Author: Elias Forsberg
+ *
+ * Created on December 26, 2012, 4:20 PM
+ */
+
+#ifndef POINTVECTOR_CPP
+#define POINTVECTOR_CPP
+
+#include "PointVector.h"
+#include <math.h>
+#include <cassert>
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>::PointVector():
+	comp {}
+{}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>::PointVector(Numeric composants[Dim]){
+	std::copy(composants, composants+Dim, comp);
+}
+
+template<int Dim, typename Numeric>
+template<int D2>
+PointVector<Dim, Numeric>::PointVector(PointVector<D2> orig):
+	comp {}
+{
+	constexpr int m = std::min(Dim, D2);
+	std::copy(orig.comp, orig.comp + m, comp);
+}
+
+template<int Dim, typename Numeric>
+template<typename... Tail>
+PointVector<Dim, Numeric>::PointVector(Tail... t){
+	PointVector();
+	pconstruct(0, t...);
+}
+
+template<int Dim, typename Numeric>
+template<typename First, typename... Tail>
+inline void PointVector<Dim, Numeric>::pconstruct(int i, First f, Tail... t){
+	comp[i] = f;
+	pconstruct(i+1, t...);
+}
+
+template<int Dim, typename Numeric>
+inline void PointVector<Dim, Numeric>::pconstruct(int /*i*/){}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::get(int i) const{
+	assert(i <= Dim);
+	return comp[i];
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::getdx() const{
+	static_assert(Dim >= 1, "This PointVector does not have an x-component");
+    return comp[0];
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::getdy() const{
+    static_assert(Dim >= 2, "This PointVector does not have a y-component");
+    return comp[1];
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::getdz() const{
+    static_assert(Dim >= 3, "This PointVector does not have a z-component");
+    return comp[2];
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::getMagnitude() const{
+	Numeric mag = 0;
+	for(int i = 0; i < Dim; i++){
+		mag += comp[i] * comp[i];
+	}
+	return sqrt(mag);
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::sum_comp() const{
+	Numeric sum = 0;
+	for(auto x : comp){
+		sum += x;
+	}
+	return sum;
+}
+
+template<int Dim, typename Numeric>
+void PointVector<Dim, Numeric>::set(int i, Numeric d){
+	assert(i <= Dim);
+	comp[i] = d;
+}
+
+template<int Dim, typename Numeric>
+void PointVector<Dim, Numeric>::setdx(Numeric d){
+	static_assert(Dim >= 1, "This PointVector does not have an x-component");
+	comp[0] = d;
+}
+
+template<int Dim, typename Numeric>
+void PointVector<Dim, Numeric>::setdy(Numeric d){
+	static_assert(Dim >= 2, "This PointVector does not have a y-component");
+	comp[1] = d;
+}
+
+template<int Dim, typename Numeric>
+void PointVector<Dim, Numeric>::setdz(Numeric d){
+	static_assert(Dim >= 3, "This PointVector does not have a z-component");
+	comp[2] = d;
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric> PointVector<Dim, Numeric>::operator +(const PointVector<Dim, Numeric> r) const{
+	return PointVector<Dim, Numeric>::add(*this, r);
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric> PointVector<Dim, Numeric>::operator -(const PointVector<Dim, Numeric> r) const{
+	return PointVector<Dim, Numeric>::sub(*this, r);
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric> PointVector<Dim, Numeric>::operator -() const{
+	return PointVector<Dim, Numeric>::mul(-1, *this);
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::operator +=(const PointVector<Dim, Numeric> r){
+	this->add(r);
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::operator -=(const PointVector<Dim, Numeric> r){
+	this->sub(r);
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::operator *=(const Numeric r){
+	this->mul(r);
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+inline PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::operator /=(const Numeric r){
+	this->div(r);
+	return *this;
+}
+
+template<int D, typename Num>
+inline PointVector<D, Num> operator*(const PointVector<D, Num> l, const Num r){
+	return PointVector<D, Num>::mul(r, l);
+}
+
+template<int D, typename Num>
+inline PointVector<D, Num> operator*(const Num l, const PointVector<D, Num> r){
+	return PointVector<D, Num>::mul(l, r);
+}
+
+template<int D, typename Num>
+inline PointVector<D, Num> operator/(const PointVector<D, Num> l, const Num r){
+	return PointVector<D, Num>::div(r, l);
+}
+
+template<int D, typename Num>
+inline PointVector<D, Num> operator/(const Num l, const PointVector<D, Num> r){
+	PointVector<D, Num> a;
+	for(int i = 0; i < D; i++){
+		a.comp[i] = 1.0/r.comp [i];
+	}
+	return PointVector<D, Num>::mul_comp(a, l);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::add(PointVector<Dim, Numeric> p){
+	for(int i = 0; i < Dim; i++){
+		comp[i] += p.comp[i];
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::sub(PointVector<Dim, Numeric> p){
+	for(int i = 0; i < Dim; i++){
+		comp[i] -= p.comp[i];
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::add(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	return p.add(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::sub(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	return p.sub(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::mul_comp(PointVector<Dim, Numeric> p){
+	for(int i = 0; i < Dim; i++){
+		comp[i] *= p.comp[i];
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::mul_cross(PointVector<Dim, Numeric> p){
+	return Cross_product(*this, p);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::mul(Numeric d){
+	for(int i = 0; i < Dim; i++){
+		comp[i] *= d;
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::mul_dot(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	Numeric result = 0;
+	for(int i = 0; i < Dim; i++){
+		result += p.comp[i] * q.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::mul_comp(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	return p.mul_comp(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::mul_cross(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	return Cross_product(p,q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::mul(Numeric d, PointVector<Dim, Numeric> p){
+	return p.mul(d);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::div_comp(PointVector<Dim, Numeric> p){
+    for(int i = 0; i < Dim; i++){
+		comp[i] /= p.comp[i];
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::div(Numeric d){
+	for(int i = 0; i < Dim; i++){
+		comp[i] /= d;
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::div_comp(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> q){
+	return p.div_comp(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::div(Numeric d, PointVector p){
+	return p.div(d);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::pow(Numeric d){
+	for(int i = 0; i < Dim; i++){
+		comp[i] = pow(comp[i], d);
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::pow_comp(PointVector p){
+	for(int i = 0; i < Dim; i++){
+		comp[i] = pow(comp[i], p.comp[i]);
+	}
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::pow(Numeric d, PointVector p){
+	return p.pow(d);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::pow_comp(PointVector p, PointVector q){
+	return p.pow_comp(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::set_min_comp(PointVector<Dim, Numeric> p){
+	for(int i = 0; i < Dim; i++){
+		*(comp+i) = *(comp+i) < *(p.comp+i) ? *(comp+i) : *(p.comp+i);
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::set_max_comp(PointVector<Dim, Numeric> p){
+	for(int i = 0; i < Dim; i++){
+		*(comp+i) = *(comp+i) > *(p.comp+i) ? *(comp+i) : *(p.comp+i);
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::min_comp(PointVector p, PointVector q){
+	return p.set_min_comp(q);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::max_comp(PointVector p, PointVector q){
+	return p.set_max_comp(q);
+}
+
+template<int Dim, typename Numeric>
+bool PointVector<Dim, Numeric>::is_min_comp(PointVector<Dim, Numeric> p) const{
+	for(int i = 0; i < Dim; i++){
+		if(comp[i] > p.comp[i])
+			return false;
+	}
+	return true;
+}
+
+template<int Dim, typename Numeric>
+bool PointVector<Dim, Numeric>::is_max_comp(PointVector<Dim, Numeric> p) const{
+	for(int i = 0; i < Dim; i++){
+		if(comp[i] < p.comp[i])
+			return false;
+	}
+	return true;
+}
+
+template<int Dim, typename Numeric>
+bool PointVector<Dim, Numeric>::is_eq_comp(PointVector<Dim, Numeric> p) const{
+	for(int i = 0; i < Dim; i++){
+		if(comp[i] != p.comp[i])
+			return false;
+	}
+	return true;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::make_unit(){
+	Numeric mag = getMagnitude();
+	for(int i = 0; i < Dim; i++){
+		comp[i] /= mag; 
+	}
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::make_unit(PointVector<Dim, Numeric> p){
+	return p.make_unit();
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::reflect(PointVector<Dim, Numeric> normal){
+	PointVector<Dim, Numeric> p = *this;
+	normal.make_unit();
+	*this = 2.0 * mul_dot(p, normal) * normal - p;
+	return *this;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>& PointVector<Dim, Numeric>::project(PointVector p){
+	PointVector<Dim, Numeric>& res = *this;
+	mul_dot(res, p)/mul_dot(p,p) * p;
+	return res;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::reflect(PointVector<Dim, Numeric> p, PointVector<Dim, Numeric> normal){
+	return p.reflect(normal);
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric> PointVector<Dim, Numeric>::project(PointVector p, PointVector q){
+	return p.project(q);
+}
+
+template<int Dim, typename Numeric>
+Numeric PointVector<Dim, Numeric>::taxicab_distance(PointVector p, PointVector q){
+	Numeric res;
+	for(int i = 0; i < Dim; i++){
+		res += abs(p.comp[i] - q.comp[i]);
+	}
+	return res;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator ==(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] == p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator !=(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] != p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator >(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] > p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator <(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] < p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator >=(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] >= p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+std::array<bool, Dim> PointVector<Dim, Numeric>::operator <=(PointVector<Dim, Numeric> p) const{
+	std::array<bool, Dim> result;
+	for(int i = 0; i < Dim; i++){
+		result[i] = comp[i] <= p.comp[i];
+	}
+	return result;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>::Cross_product::Cross_product(PointVector<Dim, Numeric> t, PointVector<Dim, Numeric> u){
+	q = t;
+	p = u;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>::Cross_product::operator PointVector<3, Numeric> () const{
+	PointVector<3, Numeric> res;
+	res.comp[0] = q.comp[1] * p.comp[2] - q.comp[2] * p.comp[1];
+	res.comp[1] = q.comp[2] * p.comp[0] - q.comp[0] * p.comp[2];
+	res.comp[2] = q.comp[0] * p.comp[1] - q.comp[1] * p.comp[0];
+	return res;
+}
+
+template<int Dim, typename Numeric>
+PointVector<Dim, Numeric>::Cross_product::operator PointVector<7, Numeric> () const{
+	PointVector<7, Numeric> res;
+	res.comp[0] = q.comp[1] * p.comp[3] - q.comp[3] * p.comp[1] + q.comp[2] * p.comp[6] - q.comp[6] * p.comp[2] + q.comp[4] * p.comp[5] - q.comp[5] * p.comp[4];
+	res.comp[1] = q.comp[2] * p.comp[4] - q.comp[4] * p.comp[2] + q.comp[3] * p.comp[0] - q.comp[0] * p.comp[3] + q.comp[5] * p.comp[6] - q.comp[6] * p.comp[5];
+	res.comp[2] = q.comp[3] * p.comp[5] - q.comp[5] * p.comp[3] + q.comp[4] * p.comp[1] - q.comp[1] * p.comp[4] + q.comp[6] * p.comp[0] - q.comp[0] * p.comp[6];
+	res.comp[3] = q.comp[4] * p.comp[6] - q.comp[6] * p.comp[4] + q.comp[5] * p.comp[2] - q.comp[2] * p.comp[5] + q.comp[0] * p.comp[1] - q.comp[1] * p.comp[0];
+	res.comp[4] = q.comp[5] * p.comp[0] - q.comp[0] * p.comp[5] + q.comp[6] * p.comp[3] - q.comp[3] * p.comp[6] + q.comp[1] * p.comp[2] - q.comp[2] * p.comp[1];
+	res.comp[5] = q.comp[6] * p.comp[1] - q.comp[1] * p.comp[6] + q.comp[0] * p.comp[4] - q.comp[4] * p.comp[0] + q.comp[2] * p.comp[3] - q.comp[3] * p.comp[2];
+	res.comp[6] = q.comp[0] * p.comp[2] - q.comp[2] * p.comp[0] + q.comp[1] * p.comp[5] - q.comp[5] * p.comp[1] + q.comp[3] * p.comp[4] - q.comp[4] * p.comp[3];
+	return res;
+}
+
+template<int Dim, typename Numeric>
+bool Boolarr::all(std::array<bool,Dim> a){
+	for(int i = 0; i < Dim; i++){
+		if(!a[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+template<int Dim, typename Numeric>
+bool Boolarr::any(std::array<bool,Dim> a){
+	for(int i = 0; i < Dim; i++){
+		if(a[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+template<int Dim, typename Numeric>
+bool Boolarr::most(std::array<bool,Dim> a){
+	int temp = 0;
+	for(int i = 0; i < Dim; i++){
+		if(a[i]){
+			temp++;
+		}
+	}
+	return temp >= Dim/2.0;
+}
+
+#endif /*POINTVECTOR_CPP*/
